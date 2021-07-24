50d58f35f11001679e678de70a4d13670a6196c1
Arthur <arthur.bowers@phabrix.com>
2021-07-24 22:41:02 +0100

    Adding class to draw and remove ships.

---
+++ bs/shipDraw.py
@@ -0,0 +1,8 @@
+"""Ship Draw class to draw the ships and remove them as the game goes on."""
+
+
+import gameBoard
+class ShipDraw:
+    """Class to place and remove ships as the game starts and goes on."""
+
+
