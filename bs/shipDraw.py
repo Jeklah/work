d6bbc0305c1a21b1e27295f99274560605604c17
Arthur <arthur.bowers@phabrix.com>
2021-07-24 23:21:40 +0100

    Started shipDraw...w.i.p

--- bs/shipDraw.py
+++ bs/shipDraw.py
@@ -1,8 +1,14 @@
 """Ship Draw class to draw the ships and remove them as the game goes on."""
 
+import Ship
+import Player
 
-import gameBoard
+
 class ShipDraw:
     """Class to place and remove ships as the game starts and goes on."""
 
+    def startGame(player1=Player.playerID, player2=Player.playerID, gameboard):
+        for ship in player1.ships:
+            if gameBoard.posX == ship.posX and gameBoard.posY == ship.posY:
+                print('*')
 
