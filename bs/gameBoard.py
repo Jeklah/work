3a5ecaba8740adf63843265413fd2c2b77c24e32
Arthur <arthur.bowers@phabrix.com>
2021-07-24 23:20:46 +0100

    Removed unnecessary code.

--- bs/gameBoard.py
+++ bs/gameBoard.py
@@ -4,10 +4,8 @@ class GameBoard:
     """Game board class to represent the game area for where the
     ships can be placed in the battleships game."""
 
-    def __init__(self, shipDrawer, gameArea):
-        self.gameArea = gameArea
-
-        self.drawBoard(self.gameArea)
+    def __init__(self, shipDrawer):
+        self.drawBoard(10)
 
     def drawBoard(area):
         for _ in range(area):
