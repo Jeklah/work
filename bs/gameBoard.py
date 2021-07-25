fe12b5a2ab44266fd73cbe66017bdd5b6009e79f
Arthur <arthur.bowers@phabrix.com>
2021-07-25 12:48:54 +0100

    Changed gameboard so it drew the board better.

--- bs/gameBoard.py
+++ bs/gameBoard.py
@@ -4,9 +4,16 @@ class GameBoard:
     """Game board class to represent the game area for where the
     ships can be placed in the battleships game."""
 
-    def __init__(self, shipDrawer):
+    def __init__(self, shipDrawer, boardGrid):
         self.drawBoard(10)
+        self.boardGrid = [10][10]
 
     def drawBoard(area):
         for _ in range(area):
-            print('~~~~~~~~~~')
+            for boardX in range(area):
+                if boardX == 10:
+                    print('~')
+                else:
+                    print('~ ', end = '')
+
+
