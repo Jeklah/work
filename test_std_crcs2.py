7f25ac27bafd7591d84c5655bd7afc8e298f9b13
Arthur <arthur.bowers@phabrix.com>
2021-10-13 17:27:10 +0100

    Updating second implementation of test.

--- test_std_crcs2.py
+++ test_std_crcs2.py
@@ -1,6 +1,7 @@
 """
 A suite of tests to check the generated standards match the CRCs we have stored on file.
 """
+import os
 import pdb
 import logging
 import time
@@ -12,8 +13,13 @@ from test_system.logconfig import test_system_log
 from test_system.models.qxseries.io import SDIIOType
 from test_system.models.qxseries.operationmode import OperationMode
 
+# Set up logging
 log = logging.getLogger(test_system_log)
 
+# Get ENV variables
+generator = os.getenv('GENERATOR_QX')
+analyser = os.getenv('ANALYSER_QX')
+test = os.getenv('TEST_QX')
 
 @pytest.fixture(scope='module')
 def generator_qx(test_generator_hostname):
@@ -21,17 +27,17 @@ def generator_qx(test_generator_hostname):
     Creates generator qx object using test_generator_hostname global. All tests in this
     suite are SDI tests and require the bouncing box and output copy to be disabled.
     """
-    gen_qx = make_qx(hostname=test_generator_hostname)
-    gen_qx.request_capability(OperationMode.SDI)
-    gen_qx.generator.bouncing_box = False
-    gen_qx.generator.output_copy = False
-    gen_qx.generator.jitter_insertion("Disabled", 0.01, 10)
-    gen_qx.io.sdi_output_source = SDIIOType.BNC
-    gen_qx.io.set_sdi_outputs(('generator', 'generator', 'generator', 'generator'))
-    log.info(f"FIXTURE: Qx {gen_qx.hostname} setup complete.")
-    yield gen_qx
-    gen_qx.generator.bouncing_box = False
-    log.info(f"FIXTURE: Qx {gen_qx.hostname} teardown complete.")
+    generator_qx = make_qx(hostname=test_generator_hostname)
+    generator_qx.request_capability(OperationMode.SDI)
+    generator_qx.generator.bouncing_box = False
+    generator_qx.generator.output_copy = False
+    generator_qx.generator.jitter_insertion("Disabled", 0.01, 10)
+    generator_qx.io.sdi_output_source = SDIIOType.BNC
+    generator_qx.io.set_sdi_outputs(('generator', 'generator', 'generator', 'generator'))
+    log.info(f"FIXTURE: Qx {generator_qx.hostname} setup complete.")
+    yield generator_qx
+    generator_qx.generator.bouncing_box = False
+    log.info(f"FIXTURE: Qx {generator_qx.hostname} teardown complete.")
 
 
 @pytest.fixture(scope='module')
@@ -55,75 +61,77 @@ def load_goldenmaster():
     return golden_master
 
 
-def gen_std_list(gen_qx, stds='fast'): # Adding this method to generate list of standards from the unit
-    if stds == 'nightly': # Nightly filter, requirements agreed upon in the meeting.
-        standards_list = gen_qx.generator.get_matching_standards(
-            [1.5],
-            r"720.*|1920.*|2048.*|3840.*|4096.*",
-            r"RGB.*|YCbCr:422:10|YCbCr:422:12|YCbCr:444:.*",
-            r".*709|.*2020|HLG.*|PQ.*}S-Log3.*",
-        )
-    elif stds == 'test':
-        standards_list = gen_qx.generator.get_matching_standards(
-            [1.5],
-            r"1920.*",
-            r"YCbCr:422:10",
-            r".*709"
-        )
-    elif stds == 'fast': # This filter was added to speed up development. It returns 5 standards.
-        standards_list = gen_qx.generator.get_matching_standards(
-            [1.5, 3.0],
-            r"(1920x1080|1280x720)[i|p]50",
-            r"YCbCr:422:10",
-            r".Rec.709"
-        )
-    elif stds == 'confidence_test_standards': # This is the same filter as found in conftest.py
-        standards_list = gen_qx.generator.get_matching_standards(
-            [1.5, 3.0, 6.0, 12.0],
-            r"\d+x\d+p\d+",
-            r"YCbCr:422:10",
-            r".*709"
-        )
-    elif stds == 'all':
-        all_stds = gen_qx.generator.get_standards()
-        standards_list = [
-            [data_rate, resolution, colour_map, gamut]
-            for data_rate in all_stds
-            for resolution in all_stds[data_rate]
-            for colour_map in all_stds[data_rate][resolution]
-            for gamut in all_stds[data_rate][resolution][colour_map]
-        ]
-    else:
-        print('stds filter was not set')
-    return standards_list
+#def gen_std_list(generator_qx, stds='fast'): # Adding this method to generate list of standards from the unit
+#    if stds == 'nightly': # Nightly filter, requirements agreed upon in the meeting.
+#        standards_list = generator_qx.generator.get_matching_standards(
+#            [1.5],
+#            r"720.*|1920.*|2048.*|3840.*|4096.*",
+#            r"RGB.*|YCbCr:422:10|YCbCr:422:12|YCbCr:444:.*",
+#            r".*709|.*2020|HLG.*|PQ.*}S-Log3.*",
+#        )
+#    elif stds == 'test':
+#        standards_list = generator_qx.generator.get_matching_standards(
+#            [1.5],
+#            r"1920.*",
+#            r"YCbCr:422:10",
+#            r".*709"
+#        )
+#    elif stds == 'fast': # This filter was added to speed up development. It returns 5 standards.
+#        standards_list = generator_qx.generator.get_matching_standards(
+#            [1.5, 3.0],
+#            r"(1920x1080|1280x720)[i|p]50",
+#            r"YCbCr:422:10",
+#            r".Rec.709"
+#        )
+#    elif stds == 'confidence_test_standards': # This is the same filter as found in conftest.py
+#        standards_list = generator_qx.generator.get_matching_standards(
+#            [1.5, 3.0, 6.0, 12.0],
+#            r"\d+x\d+p\d+",
+#            r"YCbCr:422:10",
+#            r".*709"
+#        )
+#    elif stds == 'all':
+#        all_stds = generator_qx.generator.get_standards()
+#        standards_list = [
+#            [data_rate, resolution, colour_map, gamut]
+#            for data_rate in all_stds
+#            for resolution in all_stds[data_rate]
+#            for colour_map in all_stds[data_rate][resolution]
+#            for gamut in all_stds[data_rate][resolution][colour_map]
+#        ]
+#    else:
+#        print('stds filter was not set')
+#    return standards_list
 
 
 #@pytest.fixture(scope='module')
-def get_std_and_pattern_params(gen_qx, standards_list):
+def get_std_and_pattern_params(generator_qx, confidence_test_standards):
     """
     Retrive the standard parameters from unit being tested to parametrize tests with.
     """
+    breakpoint()
     crcs = []
+    standards_list = confidence_test_standards
     for row in standards_list:
         data_rate, res, mapping, gamut = row  #golden_master.get('Standard')[row][0]
-        test_patterns = gen_qx.generator.get_test_pattern(row[0], row[1], row[2])
+        test_patterns = generator_qx.generator.get_test_pattern(row[0], row[1], row[2])
         for pattern in test_patterns:
-            crc_resp = gen_qx.analyser.get_crc_analyser() # this needs to be retrieved from golden master!
+            crc_resp = generator_qx.analyser.get_crc_analyser() # this needs to be retrieved from golden master!
             crcs = [x.get('activePictureCrc', None) for x in crc_resp] # this is derived from the line above
             yield data_rate, res, mapping, gamut, pattern, crcs   #here
 
 
-@pytest.mark.parametrize("data_rate, res, mapping, gamut, pattern", get_std_and_pattern_params(generator_qx, gen_std_list(generator_qx, stds='fast')))
+@pytest.mark.parametrize("data_rate,res,mapping,gamut,pattern", get_std_and_pattern_params(generator_qx, confidence_test_standards))#gen_std_list(generator_qx, stds='fast')))
 def test_generate_test_pattern(generator_qx, analyser_qx, data_rate, res, mapping, gamut, pattern):
     """
     Generate a given test_pattern for a given standard and checks it is generating
     what is requested.
     """
-    gen_qx.generator.set_generator(res, mapping, gamut, pattern)
-    assert gen_qx.generator.generator_status.get('pattern', None) == pattern
+    generator_qx.generator.set_generator(res, mapping, gamut, pattern)
+    assert generator_qx.generator.generator_status.get('pattern', None) == pattern
 
 
-@pytest.mark.parametrize("data_rate, res, mapping, gamut, pattern, crcs", get_std_and_pattern_params(generator_qx, gen_std_list(generator_qx, stds='fast')))
+@pytest.mark.parametrize("data_rate,res,mapping,gamut,pattern,crcs", get_std_and_pattern_params(generator_qx, confidence_test_standards))#gen_std_list(generator_qx, stds='fast')))
 def test_pattern_crc(generator_qx, analyser_qx, data_rate, res, mapping, gamut, pattern, crcs):
     """
     Checks that the generated test_pattern/s has the expected crc/s
@@ -136,9 +144,9 @@ def test_pattern_crc(generator_qx, analyser_qx, data_rate, res, mapping, gamut, 
     * Check the test pattern in the generator status is the requested pattern
     * Get the active frame CRCs from the analyser and compare to expected CRCs found in golden master
     """
-    gen_qx.generator.set_generator(res, mapping, gamut, pattern)
+    generator_qx.generator.set_generator(res, mapping, gamut, pattern)
 
-    gen_success, _, genErr = retry(10, 1, gen_qx.generator.is_generating_standard, res, mapping, gamut, pattern)
+    gen_success, _, genErr = retry(10, 1, generator_qx.generator.is_generating_standard, res, mapping, gamut, pattern)
     if not gen_success:
         pytest.fail(f"Generator didn't report generation of the standard and pattern requested. {genErr}")
 
@@ -146,12 +154,12 @@ def test_pattern_crc(generator_qx, analyser_qx, data_rate, res, mapping, gamut, 
     if not analys_success:
         pytest.fail(f"Analyser didn't report analysis of the standard and pattern requested. {analysErr}")
 
-    assert gen_qx.generator.generator_status.get('pattern', None) == pattern
+    assert generator_qx.generator.generator_status.get('pattern', None) == pattern
 
     # Check CRCs
     crc_response = []
     for _ in range(10):
-        crc_response = gen_qx.analyser.get_crc_analyser()
+        crc_response = generator_qx.analyser.get_crc_analyser()
         if crc_response[0].get('activePictureCrc', None) != "0":
             break
         time.sleep(1)
@@ -165,5 +173,3 @@ def test_pattern_crc(generator_qx, analyser_qx, data_rate, res, mapping, gamut, 
     # Check the values of the CRCs are the same as expected
     for expected_crc, recorded_crc in zip(crcs, pict_crcs):
         assert expected_crc == recorded_crc
-
-
